c831c8b296d210e7b47c6029f58455ab863315c5
longmsdu <longmsdu@163.com>
2016-06-03 14:02:19 +0800

    集成laravel-orm

---
+++ application/library/Foo/Bar.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Foo;
+
+class Bar
+{
+    public function index()
+    {
+        echo 'namespace test';
+    }
+}
---
+++ application/models/User.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Models;
+
+class User extends EloquentModel
+{
+    // 表名
+    public $table = 'user';
+}
